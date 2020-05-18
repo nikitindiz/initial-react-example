d1d7c4c944135b7f815c927ea0e7faaa1c5ca100
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-05-18 14:24:16 +0300

    Added JS expression into JSX

--- src/index.js
+++ src/index.js
@@ -3,25 +3,36 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+const cats = [
+    {
+        headerText: 'Some nice picture of cat',
+        imageUrl: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+    },
+    {
+        headerText: 'This cat\'s name is Tom',
+        imageUrl: 'https://lh3.googleusercontent.com/proxy/YanKy8j-SY4jvwVcFDyKJnurV3hR9KzNYVlYnYJoMwQhnLCIsyif7H7FjAuuPuCe6ez7CleUetDEVWIbFgCGnogwgFapAN8jwLVCjFxjP0sYABj59P-iljDO95NrbJdkAH4e993FZmdYr0pB',
+    },
+];
+
 const appContent = (
     <div className="app">
         <div className="frame">
             <h1 className="frame__header">
-                Some nice picture of cat
+                { cats[0].headerText }
             </h1>
 
             <div className="frame__content">
-                <img src="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"/>
+                <img src={ cats[0].imageUrl }/>
             </div>
         </div>
 
         <div className="frame">
             <h1 className="frame__header">
-                This cat's name is Tom
+                { cats[1].headerText }
             </h1>
 
             <div className="frame__content">
-                <img src="https://lh3.googleusercontent.com/proxy/YanKy8j-SY4jvwVcFDyKJnurV3hR9KzNYVlYnYJoMwQhnLCIsyif7H7FjAuuPuCe6ez7CleUetDEVWIbFgCGnogwgFapAN8jwLVCjFxjP0sYABj59P-iljDO95NrbJdkAH4e993FZmdYr0pB"/>
+                <img src={ cats[1].imageUrl } />
             </div>
         </div>
     </div>
