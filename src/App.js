a21342140b77f90cedf1e466dda75ab6a426880b
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-05-19 14:46:43 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -14,14 +14,20 @@ const App = () => (
 
             <a
                 href="/"
-                className="sidebar-navigation-item sidebar-navigation-item--active"
+                className={
+                    'sidebar-navigation-item ' +
+                    (window.location.search === '' && 'sidebar-navigation-item--active')
+                }
             >
                 Cats gallery
             </a>
 
             <a
                 href="/?page=traffic-lights"
-                className="sidebar-navigation-item"
+                className={
+                    'sidebar-navigation-item ' +
+                    (window.location.search === '?page=traffic-lights' && 'sidebar-navigation-item--active')
+                }
             >
                 Traffic lights
             </a>
@@ -29,35 +35,50 @@ const App = () => (
 
         <div className="main-section main-section--with-left-sidebar">
             <div className="main-section__toolbar">
-                Cats gallery
+                {window.location.search === '' && 'Cats gallery'}
+
+                {window.location.search === '?page=traffic-lights' && 'Traffic lights'}
             </div>
 
-            <div className="main-section__content">
-                <h1>
-                    Some cat pictures here :-)
-                </h1>
+            {window.location.search === '' && (
+                <div className="main-section__content">
+                    <h1>
+                        Some cat pictures here :-)
+                    </h1>
 
-                <FrameForImage
-                    header={cats[0].headerText}
-                    image={cats[0].imageUrl}
-                >
-                    {cats[0].description}
-                </FrameForImage>
+                    <FrameForImage
+                        header={cats[0].headerText}
+                        image={cats[0].imageUrl}
+                    >
+                        {cats[0].description}
+                    </FrameForImage>
 
-                <FrameForImage
-                    header={cats[1].headerText}
-                    image={cats[1].imageUrl}
-                >
-                    {cats[1].description}
-                </FrameForImage>
+                    <FrameForImage
+                        header={cats[1].headerText}
+                        image={cats[1].imageUrl}
+                    >
+                        {cats[1].description}
+                    </FrameForImage>
 
-                <FrameForImage
-                    header={cats[2].headerText}
-                    image={cats[2].imageUrl}
-                >
-                    {cats[2].description}
-                </FrameForImage>
-            </div>
+                    <FrameForImage
+                        header={cats[2].headerText}
+                        image={cats[2].imageUrl}
+                    >
+                        {cats[2].description}
+                    </FrameForImage>
+                </div>
+            )}
+
+
+            {window.location.search === '?page=traffic-lights' && (
+                <div className="main-section__content">
+                    <h1>
+                        Traffic Light component will appear here
+                    </h1>
+
+                    Not implemented yet
+                </div>
+            )}
         </div>
     </div>
 );
