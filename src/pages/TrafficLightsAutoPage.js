5be0d2cf7901ec110435852411e0ebb664e09707
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-05-22 11:45:09 +0300

    Added automatic traffic lights

---
+++ src/pages/TrafficLightsAutoPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TrafficLightsAuto from '../components/TrafficLightsAuto';
+
+const TrafficLightsPage = () => (
+    <div className="main-section__content">
+        <h1>
+            Traffic Light controlled automatically
+        </h1>
+
+        <TrafficLightsAuto />
+    </div>
+);
+
+export default TrafficLightsPage;
